eff46fa8185f75779283403202cbcb57bd4ca97c
nick <shijh302@gmail.com>
2017-10-23 12:17:25 -0500

    new  features
    new  features

--- hw6/angular-web-services/src/app/app.component.ts
+++ hw6/angular-web-services/src/app/app.component.ts
@@ -1,36 +1,11 @@
 import { Component } from '@angular/core';
-import { WikipediaSearch } from "./wikipedia-search.service";
-import { GiphyService } from "./giphy-service.service";
-import { SearchItem } from "./search-item.model";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [WikipediaSearch, GiphyService]
+
 })
 export class AppComponent {
   title = 'app';
-
-  searchText: string;
-
-  searchResult: string[] = [];
-
-  searchImageItems: string[];
-
-  constructor(private _wikipediaService: WikipediaSearch, private _giphyService: GiphyService) { this.searchText = "";}
-
-  btnSearch() {
-    this._wikipediaService.search(this.searchText).subscribe(data => {
-
-      this.searchResult = data[1];
-      this._wikipediaService.addSearchItem(new SearchItem(new Date(), data[1]));
-    });
-  }
-
-  btnGiphyApiSearch() {
-    this._giphyService.search(this.searchText).subscribe(data => {
-      console.log(data);
-      this.searchImageItems = data.data.map(item => item.images['480w_still']['url']);
-    })
-  }
 }
--- hw6/angular-web-services/src/app/home/home.component.ts
+++ hw6/angular-web-services/src/app/home/home.component.ts
@@ -1,13 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { WikipediaSearch } from "../wikipedia-search.service";
+import { GiphyService } from "../giphy-service.service";
+import { SearchItem } from "../search-item.model";
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [WikipediaSearch, GiphyService]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  searchText: string;
+
+  searchResult: string[] = [];
+
+  searchImageItems: string[];
+
+  constructor(private _wikipediaService: WikipediaSearch, private _giphyService: GiphyService) { this.searchText = "";}
+
+  btnSearch() {
+    this._wikipediaService.search(this.searchText).subscribe(data => {
+
+      this.searchResult = data[1];
+      this.searchImageItems = [];
+      this._wikipediaService.addSearchItem(new SearchItem(new Date(), data[1]));
+    });
+  }
+
+  btnGiphyApiSearch() {
+    this._giphyService.search(this.searchText).subscribe(data => {
+      this.searchResult = [];
+      this.searchImageItems = data.data.map(item => item.images['480w_still']['url']);
+    })
+  }
 
   ngOnInit() {
   }
